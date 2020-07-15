7e7273bcb5003c085b042a194647200c1ff076f6
artyom.svyatoha <6Jyc2Pp4WY>
2020-07-15 09:49:31 +0300

    version 1.1

--- cookie-cliker.js
+++ cookie-cliker.js
@@ -3,18 +3,34 @@
 // @namespace   Cookie Cliker Scripts
 // @match       https://orteil.dashnet.org/cookieclicker/
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Artem_8086
 // @description 14.07.2020, 15:54:24
 // ==/UserScript==
 
 
-var cookie = document.getElementById('bigCookie');
-var shimers = document.getElementById('shimmers')
+(function() {
 
-setInterval(function() {
-  cookie.click()
-  for (var i=0, shimer; shimer=shimers.children[i]; i++) {
-    shimer.click()
+  var cookie = document.getElementById('bigCookie')
+  var shimers = document.getElementById('shimmers')
+
+  var timer = null
+  window.enableBot = function() {
+    timer = setInterval(function() {
+      cookie.click()
+      for (var i=0, shimer; shimer=shimers.children[i]; i++) {
+        shimer.click()
+      }
+    },0)
   }
-},0);
+
+  window.disableBot = function() {
+    if (timer != null) {
+      clearInterval(timer)
+      timer = null
+    }
+  }
+
+  setTimeout(enableBot, 1000)
+
+})()
